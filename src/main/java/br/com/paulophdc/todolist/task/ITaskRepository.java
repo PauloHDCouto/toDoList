be0d8e868f3032dcb741f63210bbec6203f2b964
Paulinho <paulo.phdc.28@gmail.com>
2023-10-13 21:05:57 -0300

    ToDoLIst terceira aula

---
+++ src/main/java/br/com/paulophdc/todolist/task/ITaskRepository.java
@@ -0,0 +1,15 @@
+package br.com.paulophdc.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+	
+	
+	
+	
+}
